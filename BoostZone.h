34f62a4e44711827e840f8272b90de97f41a95bd
pauot16 <pau.gallego.guerrero@estudiantat.upc.edu>
2025-12-13 15:03:23 +0100

    Create BoostZone.h

---
+++ BoostZone.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Application.h"
+#include "BoxSensor.h"
+#include "PhysicEntity.h"
+#include "ModulePhysics.h"
+#include <cmath>
+
+class BoostZone : public BoxSensor
+{
+public:
+	BoostZone(Application* app, int _x, int _y, int width, int height, float angle, Module* _listener, float _impulse)
+		: BoxSensor(app->physics, app->renderer, _x, _y, width, height, _listener, SLOWZONE, angle, false) 
+		, boostImpulse(_impulse)
+	{
+
+	}
+
+	void OnCollision(PhysicEntity* other) {
+		if (!other || !other->body) return;
+		// Appliy impulse towards the direction of the player
+		if (other->body->body) {
+			float angle = other->body->body->GetAngle();
+			float ix = cosf(angle) * boostImpulse;
+			float iy = sinf(angle) * boostImpulse;
+			other->body->ApplyImpulse(ix, iy);
+		}
+	}
+
+	void OnCollisionEnd(PhysicEntity* other) {
+
+	}
+
+	~BoostZone() {
+		auto pbody = body->body;
+		physics->DestroyBody(pbody);
+	}
+
+	float boostImpulse;
+};
